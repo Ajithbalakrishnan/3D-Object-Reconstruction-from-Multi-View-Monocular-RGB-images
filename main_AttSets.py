7cb542b412c2b040ffc2965220bcf37bd15f75cb
Ajithbalakrishnan <ajithkannampara@gmail.com>
2019-06-14 23:50:56 +0530

    14/06/19

--- main_AttSets.py
+++ main_AttSets.py
@@ -7,7 +7,7 @@ sys.path.append('..')
 import tools as tools
 import numpy as np
 import time
-from keras.layers import BatchNormalization,Conv3D,MaxPooling3D,Dense,Reshape,Add,LeakyReLU
+from keras.layers import BatchNormalization,Conv3D,MaxPooling3D,Dense,Reshape,Add,LeakyReLU,Conv3DTranspose
 from keras.activations import relu,sigmoid
 from keras import models
 import copy 
@@ -20,7 +20,7 @@ img_res = 127
 vox_res32 = 32
 total_mv = 24   
 GPU0 = '0'
-GPU1 = '1'
+
 re_train=False
 #re_train=True
 single_view_train = True
@@ -31,12 +31,13 @@ multi_view_train = True
 config={}                                 # python dictionary
 config['batch_size'] = batch_size
 config['total_mv'] = total_mv
+config['cat_names'] = ['02691156']
 #config['cat_names'] = ['02691156','02828884','02933112','02958343','03001627','03211117',
 #            '03636649','03691459','04090263','04256520','04379243','04401088','04530566']
-config['cat_names'] = ['02828884']
+#config['cat_names'] = ['02828884']
 for name in config['cat_names']:
-    config['X_rgb_'+name] = './Data_sample/ShapeNetRendering/'+name+'/'
-    config['Y_vox_'+name] = './Data_sample/ShapeNetVox32/'+name+'/'
+    config['X_rgb_'+name] = '/home/gpu/Desktop/Ajith_Balakrishnan/Data_sample/ShapeNetRendering/'+name+'/'
+    config['Y_vox_'+name] = '/home/gpu/Desktop/Ajith_Balakrishnan/Data_sample/ShapeNetVox32/'+name+'/'
 
 # output : {'batch_size': 1, 'total_mv': 24, 'cat_names': ['03001627'], 'Y_vox_03001627': '/home/wiproec4/3d reconstruction/attsets/Data_sample/#ShapeNetVox32/03001627/', 'X_rgb_03001627': '/home/wiproec4/3d reconstruction/attsets/Data_sample/ShapeNetRendering/03001627/'}
 def metric_IoU(batch_voxel_occup_pred, batch_voxel_occup_true):
@@ -100,8 +101,9 @@ def refiner_network(volumes_in):
 		reshaped_1=Add()([fc2,volumes_4_l]) 
 		
 		print("reshaped_1.shape",reshaped_1.shape)
-		
-		rn13= tools.Ops.deconv3d(reshaped_1, k=4, out_c=64, str=2, name='ref_c4')
+
+		rn13=Conv3DTranspose(filters=64, kernel_size=(4, 4, 4), padding='same',data_format="channels_last",name='ref_c4',strides=(2, 2, 2))(reshaped_1)
+#		rn13= tools.Ops.deconv3d(reshaped_1, k=4, out_c=64, str=2, name='ref_c4')
 		rn14=BatchNormalization()(rn13)
 		volumes_4_r=relu(rn14, alpha=0.0, max_value=None, threshold=0.0)
 		
@@ -111,7 +113,8 @@ def refiner_network(volumes_in):
 		
 		print("reshaped_2_shape",reshaped_2.shape)
 
-		rn16= tools.Ops.deconv3d(reshaped_2, k=4, out_c=32, str=2, name='ref_c5')
+#		rn16= tools.Ops.deconv3d(reshaped_2, k=4, out_c=32, str=2, name='ref_c5')
+		rn16=Conv3DTranspose(filters=32, kernel_size=(4, 4, 4), padding='same',data_format="channels_last",name='ref_c5',strides=(2, 2, 2))(reshaped_2)
 		rn17=BatchNormalization()(rn16)
 		volumes_8_r =relu(rn17, alpha=0.0, max_value=None, threshold=0.0)
 	 
@@ -119,7 +122,8 @@ def refiner_network(volumes_in):
 		
 		print("reshaped_3_shape",reshaped_3.shape)
 		
-		rn19= tools.Ops.deconv3d(volumes_8_r, k=4, out_c=1, str=2, name='ref_c6')
+#		rn19= tools.Ops.deconv3d(volumes_8_r, k=4, out_c=1, str=2, name='ref_c6')
+		rn19=Conv3DTranspose(filters=1, kernel_size=(4, 4, 4), padding='same',data_format="channels_last",name='ref_c6',strides=(2, 2, 2))(volumes_8_r)
 		volumes_16_r= tf.nn.sigmoid(rn19,name='ref_sigmoid1')
 
 		reshape_4=Add()([volumes_16_r,input_volumes_32])
@@ -219,7 +223,7 @@ class Network:
 		with tf.variable_scope('Encoder'):
 			im_num = tf.shape(X_rgb)[1]
 			[_, _, d1, d2, cc] = X_rgb.get_shape()
-			X_rgb = tf.reshape(X_rgb, [-1, int(d1), int(d2), int(cc)],name="r2n_in")
+			X_rgb = tf.reshape(X_rgb, [-1, int(d1), int(d2), int(cc)],name="en_in")
 			print("Network Structure")
 			print("base_r2n2",X_rgb.shape)
 	 
@@ -254,9 +258,9 @@ class Network:
 			print("l7_r2n",l7.shape)
 			l8 = tools.Ops.xxlu(tools.Ops.conv2d(l7, k=3, out_c=en_c[3], str=1, name='en_c8'), label='lrelu')
 			print("l8_r2n",l8.shape)
-			l66=tools.Ops.conv2d(l6, k=1, out_c=en_c[3], str=1, name='en_c66')
-			print("l66_r2n",l66.shape)
-			l8=l8+l66
+#			l66=tools.Ops.conv2d(l6, k=1, out_c=en_c[3], str=1, name='en_c66')
+#			print("l66_r2n",l66.shape)
+#			l8=l8+l66
 			l8 = tools.Ops.maxpool2d(l8, k=2, s=2, name='en_mp4')
 			print("l8_r2n",l8.shape)
 
@@ -280,26 +284,28 @@ class Network:
 			l12 = tools.Ops.maxpool2d(l12, k=2, s=2, name='en_mp6')
 			print("l12_r2n",l12.shape)
 			
-		with tf.variable_scope('Att_Net'):
+		
 			[_, d1, d2, cc] = l12.get_shape()
-			l12 = tf.reshape(l12, [-1, int(d1) * int(d2) * int(cc)],name="Att_fc1_in")
+			l12 = tf.reshape(l12, [-1, int(d1) * int(d2) * int(cc)],name="en_fc1_in")
 			print("fc1_input_r2n",l12.shape)
-			fc = tools.Ops.xxlu(tools.Ops.fc(l12, out_d=1024, name='Att_fc1'), label='lrelu')
+			fc = tools.Ops.xxlu(tools.Ops.fc(l12, out_d=1024, name='en_fc1'), label='lrelu')
 			print("fc1_output_r2n",fc.shape)
 			
+		with tf.variable_scope('Att_Net'):	
 			#### use fc attention
 			input = tf.reshape(fc, [-1, im_num, 1024],name="Att_fc_in")
 			print("att_fc_in_r2n",input.shape)
 			latent_3d, weights = attsets_fc(input, out_ele_num=1)
 			print("att_fc_out_r2n",latent_3d.shape)
-
+			
+		with tf.variable_scope('Decoder'):
 			####
-			latent_3d = tools.Ops.xxlu(tools.Ops.fc(latent_3d, out_d=4*4*4*128, name='Att_fc2'), label='lrelu')
+			latent_3d = tools.Ops.xxlu(tools.Ops.fc(latent_3d, out_d=4*4*4*128, name='de_fc2'), label='lrelu')
 			print("fc3_out_r2n",latent_3d.shape)
-			latent_3d = tf.reshape(latent_3d, [-1, 4, 4, 4, 128],name="Att_fc2_out")
+			latent_3d = tf.reshape(latent_3d, [-1, 4, 4, 4, 128],name="de_fc2_out")
 
 		####
-		with tf.variable_scope('Decoder'):
+
 			de_c = [128, 128, 128, 64, 32, 1]
 			
 			print("d1_in_r2n",latent_3d.shape)
@@ -360,33 +366,32 @@ class Network:
 			tf.summary.histogram('Attsets_Weights', self.weights)
 		#for d in['0', '1'] 
 		#    with tf.device('/gpu:' + d):
-		with tf.device('/gpu:' + GPU1):
+	        #with tf.device('/gpu:' + GPU1):	
 			### rec loss
 			print ("reached")
 			with tf.variable_scope('Loss_Fun'):
 				Y_vox_ = tf.reshape(self.Y_vox, shape=[-1, vox_res ** 3])
 				Y_pred_ = tf.reshape(self.Y_pred, shape=[-1, vox_res ** 3])
 				vae_o_=tf.reshape(self.vae_o, shape=[-1, vox_res ** 3])
-				
+			
 				self.vae_loss= tf.reduce_mean(-tf.reduce_mean(Y_vox_ * tf.log(vae_o_ + 1e-8), reduction_indices=[1]) -
-									 tf.reduce_mean((1 - Y_vox_) * tf.log(1 - vae_o_ + 1e-8),reduction_indices=[1]))
+										 tf.reduce_mean((1 - Y_vox_) * tf.log(1 - vae_o_ + 1e-8),reduction_indices=[1]))
 				self.rec_loss = tf.reduce_mean(-tf.reduce_mean(Y_vox_ * tf.log(Y_pred_ + 1e-8), reduction_indices=[1]) -
-									 tf.reduce_mean((1 - Y_vox_) * tf.log(1 - Y_pred_ + 1e-8),reduction_indices=[1]))
+										 tf.reduce_mean((1 - Y_vox_) * tf.log(1 - Y_pred_ + 1e-8),reduction_indices=[1]))
 				sum_rec_loss = tf.summary.scalar('rec_loss', self.rec_loss)
 				self.sum_merged = sum_rec_loss
 				tf.summary.histogram('rec_loss', self.rec_loss)
 				tf.summary.scalar("vae-loss",self.vae_loss)
 				tf.summary.histogram("vae-loss",self.vae_loss)
-							 
-
+								 
 				base_en_var = [var for var in tf.trainable_variables() if var.name.startswith('Encoder/en')]
 				base_dec_var = [var for var in tf.trainable_variables() if var.name.startswith('Decoder/de')]
 				att_var = [var for var in tf.trainable_variables() if var.name.startswith('Att_Net/att')]
 				refine_var = [var for var in tf.trainable_variables() if var.name.startswith('ref_net/ref')]
 				
-				self.base_en_optim = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.rec_loss, var_list=base_en_var)
-				self.base_de_optim = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.rec_loss, var_list=base_dec_var)
-				self.att_optim = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.rec_loss, var_list=att_var)
+#				self.base_en_optim = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.rec_loss, var_list=base_en_var)
+#				self.base_de_optim = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.rec_loss, var_list=base_dec_var)
+#				self.att_optim = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.rec_loss, var_list=att_var)
 				self.refine_optim = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(self.rec_loss, var_list=refine_var)
 				
 				self.base_en_optim2 = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.vae_loss, var_list=base_en_var)
@@ -416,7 +421,7 @@ class Network:
 		return 0
     
 	def train(self, data):
-		for epoch in range(0, 10, 1):
+		for epoch in range(0, 500, 1):
 			train_view_num = 24  ##!!!!!!!!!!!
 			data.shuffle_train_files(epoch, train_mv=train_view_num)
 			total_train_batch_num = data.total_train_batch_num  #int(len(self.X_rgb_train_files)/(self.batch_size*train_mv))
@@ -448,28 +453,27 @@ class Network:
 					print("single_view_train_rgb_input_shape ",rgb.shape)
 					vox = np.tile(Y_vox_bat[:,None,:,:,:],[1,train_view_num,1,1,1])
 					vox = np.reshape(vox, [batch_size*train_view_num, 32,32,32])	
-					vae_loss_c,eee,ddd, rec_loss_c, sum_train,rrr,eeee,dddd = self.sess.run([self.vae_loss,self.base_en_optim,self.base_de_optim,self.rec_loss,self.merged,self.refine_optim,self.base_en_optim2,self.base_de_optim2],feed_dict={self.X_rgb: rgb, self.Y_vox: vox, self.lr: 0.0001})
+					vae_loss_c,eee,ddd, rec_loss_c, sum_train,rrr = self.sess.run([self.vae_loss,self.base_en_optim2,self.base_de_optim2,self.rec_loss,self.merged,self.refine_optim],feed_dict={self.X_rgb: rgb, self.Y_vox: vox, self.lr: 0.0001})
 					print ('ep:', epoch, 'i:', i, 'train single rec loss:', rec_loss_c)
 					print('ep:',epoch,'i',i,'train single vae loss',vae_loss_c)
                                         									
 				########## multi view train
 				if multi_view_train:
 					
-					vae_loss_c,rec_loss_c, _, sum_train,xxx,yyyy = self.sess.run([self.vae_loss,self.rec_loss, self.att_optim, self.merged,self.refine_optim,self.att_optim2],feed_dict={self.X_rgb: X_rgb_bat, self.Y_vox: Y_vox_bat,self.lr: 0.0001})
+					vae_loss_c,rec_loss_c, _, sum_train,xxx = self.sess.run([self.vae_loss,self.rec_loss, self.att_optim2, self.merged,self.refine_optim],feed_dict={self.X_rgb: X_rgb_bat, self.Y_vox: Y_vox_bat,self.lr: 0.0001})
 					print ('ep:', epoch, 'i:', i, 'train multi rec loss:', rec_loss_c)
 					print('ep:',epoch,'i',i,'train multi vae loss',vae_loss_c)
                                         				
 				############
-				if i % 2 == 0:
+				if i % 10 == 0:
 					self.sum_writer_train.add_summary(sum_train, epoch * total_train_batch_num + i)
 					
 				
 				#### testing
-				if i % 10 == 0 :
-					X_rgb_batch, Y_vox_batch = data.load_X_Y_test_next_batch(test_mv=1)
-					rec_loss_te, Y_vox_test_pred, att_pred, sum_test = \
-						self.sess.run([self.rec_loss, self.Y_pred,self.weights, self.sum_merged],
-					    feed_dict={self.X_rgb: X_rgb_batch, self.Y_vox: Y_vox_batch})
+				if i % 50 == 0 :
+					X_rgb_batch, Y_vox_batch = data.load_X_Y_test_next_batch(test_mv=3)
+					bbbb,dddd,rrrr,aaaa,rec_loss_te, qwerty, Y_vox_test_pred, att_pred, sum_test = \
+						self.sess.run([self.base_en_optim2,self.base_de_optim2,self.refine_optim,self.att_optim2,self.rec_loss,self.vae_loss, self.Y_pred,self.weights, self.merged],feed_dict={self.X_rgb: X_rgb_batch, self.Y_vox: Y_vox_batch,self.lr: 0.0001})
 					X_rgb_batch = X_rgb_batch.astype(np.float16)
 					Y_vox_batch = Y_vox_batch.astype(np.float16)
 					Y_vox_test_pred = Y_vox_test_pred.astype(np.float16)
@@ -478,9 +482,10 @@ class Network:
 					scipy.io.savemat(self.test_res_dir+'X_Y_pred_'+str(epoch).zfill(2)+'_'+str(i).zfill(5)+'.mat',to_save,do_compression=True)
 					self.sum_writer_test.add_summary(sum_test, epoch * total_train_batch_num + i)
 					print ('ep:', epoch, 'i:', i, 'test rec loss:', rec_loss_te)
+					print ('ep:', epoch, 'i:', i, 'test vae loss:', qwerty) 
 					
 				#### model saving
-				if i % 5 == 0 :
+				if i % 100 == 0 :
 					self.saver.save( self.sess, save_path=self.train_mod_dir + 'model.cptk' )
 					print ( 'epoch:', epoch, 'i:', i, 'model saved!' )
   
@@ -515,3 +520,4 @@ if __name__ =='__main__':
 	
 
 	
+
